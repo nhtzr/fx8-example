cec446e0d6918855f26b0f9de5000c3ee7962196
Ezequiel Rosas <ezequiel.rosas@autodesk.com>
2016-11-24 14:57:52 -0600

    Add keyword replacement

--- src/sample/Service.java
+++ src/sample/Service.java
@@ -1,22 +1,66 @@
 package sample;
 
-import org.apache.commons.lang3.StringUtils;
 import sample.model.QueryLine;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Created by e.rosas.garcia on 23/11/2016.
  */
 public class Service {
 
+    private Set<String> keywords = new HashSet<>(Arrays.asList("select", "from", "where"));
+    private Pattern keywordPattern = Pattern.compile("^\\s*(\\w+)");
+
     public String buildSqlQuery(Collection<QueryLine> lines) {
-        StringBuilder sqlQuery = new StringBuilder();
-        lines.stream()
+        final Set<String> keyWordsUsed = new HashSet<>();
+        String sqlQuery = lines.stream()
                 .filter(QueryLine::isEnabled)
                 .map(QueryLine::getLine)
-                .forEach(sqlQuery::append);
-        return StringUtils.normalizeSpace(sqlQuery.toString());
+                .map(String::trim)
+                .map(line -> processKeyWords(line, keyWordsUsed))
+                .collect(Collectors.joining("\n"));
+        return sqlQuery;
+    }
+
+
+    public String processKeyWords(String line, Set<String> keyWordsUsed) {
+        String keyword = keywordOf(line);
+        if (keyword == null) return line;
+        if (keyWordsUsed.contains(keyword)) {
+            return line.replaceFirst(keyword, replacementOf(keyword));
+        }
+        keyWordsUsed.add(keyword);
+        return line;
+    }
+
+    private String keywordOf(String line) {
+        Matcher matcher = keywordPattern.matcher(line);
+        if (!matcher.find()) return null;
+
+        String keyword = matcher.group();
+        if (!keywords.contains(keyword)) return null;
+
+        return keyword;
+    }
+
+    private String replacementOf(String keyword) {
+        switch (keyword) {
+            case "select":
+                return ",";
+            case "from":
+                return "from";
+            case "where":
+                return "and";
+            default:
+                return keyword;
+        }
     }
 
 }
